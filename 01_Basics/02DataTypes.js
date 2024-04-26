97e6e9af98a2de8c66364cd7eab85a0a1092b781
Jyoti634 <163453673+Jyoti634@users.noreply.github.com>
2024-04-26 00:20:20 +0000

    data type

---
+++ 01_Basics/02DataTypes.js
@@ -0,0 +1,26 @@
+/* use strict - treat all the JS code as newer version 
+alert statement - we will get alert, but in node js env . we use alert statement differently 
+Semicolons avoid i node and react and Java script ,\
+code readability should be high
+
+*/
+let name = "Jyoti"
+let age = 26
+let isLoggedIn = false
+
+/*number => 2 to power 53
+bigInt 
+String => ""
+boolean => true or false 
+null => standalone value , representation of an empty value 
+undefined => variable declared but not value assigned . 
+symbol = > unique value 
+nono premitive : 
+Object . 
+String 
+
+
+ */
+console.log(typeof"hitesh");// string 
+console.log(typeof undefined);//undefinded
+console.log(typeof null);// object
