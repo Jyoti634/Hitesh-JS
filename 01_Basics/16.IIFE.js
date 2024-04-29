772d68b14d70975fd5bbe5b8f37c45682605ebf9
Jyoti634 <163453673+Jyoti634@users.noreply.github.com>
2024-04-29 19:02:28 +0000

    IIFE

---
+++ 01_Basics/16.IIFE.js
@@ -0,0 +1,24 @@
+// function chai(){
+//     console.log(`DB CONNECTED`);
+// }
+// chai()//DB CONNECTED
+
+// (function chai(){
+//     console.log(`DB CONNECTED`);
+// })();//DB CONNECTED
+
+// (function aurcode(){
+//     console.log(`DB CONNECTED TWO`)
+// })()//DB CONNECTED TWO
+
+// (() => {
+//     console.log(`DB CONNECTED`);
+// })();//DB CONNECTED
+
+// (() => {
+//     console.log(`DB CONNECTED TWO`)
+// })()//DB CONNECTED TWO
+
+((name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+})("hitesh")//DB CONNECTED TWO hitesh
